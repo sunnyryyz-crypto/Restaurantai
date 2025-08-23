39d1a5b9aa62bbf1071efdb1b387f0e361546a44
sunnyryyz-crypto <sunnyryyz@gmail.com>
2025-08-23 19:21:24 -0400

    Create menu.py

---
+++ app/routes/menu.py
@@ -0,0 +1,8 @@
+from . import db
+
+class MenuItem(db.Model):
+    __tablename__ = 'menu_items'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
